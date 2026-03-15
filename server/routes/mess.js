8d5f4ab9bfa8dc1a3a407fb1574384d5e91111a6
Hrushii27 <hrushijagtap5.dypcet@dypgroup.edu.in>
2026-03-16 01:00:25 +0530

    Fix: Add missing body import in mess.js

--- server/routes/mess.js
+++ server/routes/mess.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Mess = require('../models/mess');
 const Subscription = require('../models/subscription');
+const { body } = require('express-validator');
 const { validateRequest } = require('../middleware/validation');
 
 const messValidation = [
