6adea70731d4009a99277503dd0bee36f94dd39b
Hrushii27 <hrushijagtap5.dypcet@dypgroup.edu.in>
2026-03-16 10:42:14 +0530

    fix: broaden CSP rules for Razorpay and reCAPTCHA framing

--- server/server.js
+++ server/server.js
@@ -33,8 +33,8 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'", "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", "https://checkout.razorpay.com"],
-        frameSrc: ["'self'", "https://www.google.com/recaptcha/", "https://recaptcha.google.com/", "https://checkout.razorpay.com"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", "https://checkout.razorpay.com", "https://api.razorpay.com"],
+        frameSrc: ["'self'", "https://www.google.com/", "https://recaptcha.google.com/", "https://checkout.razorpay.com/", "https://api.razorpay.com/"],
         connectSrc: ["'self'", "https://www.google.com/recaptcha/", "https://api.findmess.me", "https://api.razorpay.com"],
         imgSrc: ["'self'", "data:", "https://www.gstatic.com/", "https://www.google.com/"],
         styleSrc: ["'self'", "'unsafe-inline'"],
