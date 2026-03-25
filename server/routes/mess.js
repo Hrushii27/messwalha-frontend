20bdd238cdc0a67822d5bc0a6d1dc82c25928acf
Hrushii27 <hrushikeshjagtap143@gmail.com>
2026-03-25 19:31:04 +0530

    Implement Menus and Reviews functionality

--- server/routes/mess.js
+++ server/routes/mess.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/db');
 const Mess = require('../models/mess');
 const Subscription = require('../models/subscription');
 const { body } = require('express-validator');
